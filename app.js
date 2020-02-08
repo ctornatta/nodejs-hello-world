b3a99b62259ac7116a4521fb92f95b96a8770d89
Chris Tornatta <chris@tornatta.net>
2020-02-07 20:41:30 -0500

    work in progress...

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var app = express();
+var redis = require('redis');
 
 
 
 app.get('/', function (req, res) {
 
-  res.send('Hello World');
+  var client = redis.createClient("redis://127.0.0.1:6379");
+
+  client.on('connect', function() {
+    res.send('Redis client connected');
+  });
+  
+  client.on('error', function (err) {
+    res.send('Something went wrong ' + err);
+  });
+
+  client.set('my test key', 'my test value', redis.print);
+  client.get('my test key', function (error, result) {
+      if (error) {
+          console.log(error);
+          throw error;
+      }
+      console.log('GET result ->' + result);
+  });
+
+  //res.send('Hello World');
   console.log(req.headers);
 
 })
