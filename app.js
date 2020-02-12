dbbf3584d89e241dedf189c66b7aacb48004e2e7
Chris Tornatta <ctornatta@lilly.com>
2020-02-12 08:34:44 -0500

    work in progress...

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 var express = require('express');
 var app = express();
 var redis = require('redis');
+const axios = require('axios');
 
 app.get('/', function (req, res) {
 
   //process.env.REDIS_URL || 'redis://127.0.0.1:6379'
   //var client = redis.createClient("redis://127.0.0.1:6379");
+
+  // axios.get('http://icanhazip.com')
+  // .then(function (response) {
+  //   // handle success
+  //   console.log(response);
+  // })
+  // .catch(function (error) {
+  //   // handle error
+  //   console.log(error);
+  // })
+  // .then(function () {
+  //   console.log('done')
+  // });
+
   var client = redis.createClient(process.env.REDIS_URL || 'redis://127.0.0.1:6379');
 
   client.on('connect', function() {
-    res.send('Redis client connected');
+  // Make a request for a user with a given ID
+    res.send('Redis client connected\n' + process.env.MY_SECRET);
   });
   
   client.on('error', function (err) {
